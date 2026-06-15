83495fc70cb05fe87796ab28636b14dd09ee612f
flex <bosssss.5000@gmail.com>
2026-06-15 23:27:37 +0200

    feat(mariadb): add Docker setup with entry script and config
    - working standalone mariadb Dockerfile and config
    - basic compose file for mariadb service

---
+++ srcs/requirements/mariadb/docker-entry.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# changes behaviour of shell failures
+# -e
+#   shell exits if any command fails (does not change the pipe behaviour, where only the last command determines the outcome)
+# -u:
+#   expansions fail with consequences (also prints error message)
+#
+# In combination they make the script exit/stop if any error happens along the way.
+# - command failure
+# - variable expansion
+set -eu
+
+echo "[MARIADB ENTRY SCRIPT] executing..."
+
+if [ ! -d "${MARIADB_DATADIR}/mysql" ]; then
+	echo "[MARIADB ENTRY SCRIPT] no database found, creating ..."
+	mariadb-install-db \
+		--defaults-file="${MARIADB_CONFIG}" \
+		--datadir="${MARIADB_DATADIR}" \
+		--user=mysql \
+		--skip-test-db
+else
+	echo "[MARIADB ENTRY SCRIPT] found database"
+fi
+
+echo "[MARIADB ENTRY SCRIPT] finished."
+
+exec "$@"
